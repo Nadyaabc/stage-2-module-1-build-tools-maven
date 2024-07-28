247116a80aada4320d600245657ba4077b74da49
Nadejda <nadyash295923@gmail.com>
2024-07-28 19:18:09 +0300

    feat: do task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for(String s:args){
-            if (Double.parseDouble(s) < 1)return false;
+        try {
+            for (String arg : args) {
+                double num = Double.parseDouble(arg);
+                if (num <= 0) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
-        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Double.parseDouble(str) > 0;
+        try {
+            double num = Double.parseDouble(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
